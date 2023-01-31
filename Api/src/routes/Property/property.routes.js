14f552ae6c0498f2ddcad4880bddd11796e260f3
JuandiAndrade <juandi.andrade.g@gmail.com>
2023-01-31 19:00:20 -0300

    creacion property, cambios en model property y app

---
+++ Api/src/routes/Property/property.routes.js
@@ -0,0 +1,7 @@
+import { createProperty } from "../../controllers/Property/property.controller.js";
+import { Router } from "express";
+  
+  const router = Router();
+  
+  router.post("/property", createProperty);
+  export default router;
