944eaf849e61c77086e404e84a6ddc951321f5b4
Luciano Sanchez <cbrook3n@gmail.com>
2023-02-01 17:20:24 -0300

    client model actualizado

--- Api/src/models/aboutme.model.js
+++ Api/src/models/aboutme.model.js
@@ -9,14 +9,54 @@ export const Aboutme = sequelize.define("Aboutmes", {
   },
   description: {
     type: DataTypes.TEXT,
+    validator: {
+      len: {
+        args: [3, 300],
+        msg: "Description needs to have between 3 to 300 characters",
+      },
+      notNull: {
+        args: true,
+        msg: "Cannot be null",
+      },
+    },
   },
   hobbies: {
     type: DataTypes.ARRAY(DataTypes.STRING),
   },
   age: {
     type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isInt: {
+        args: true,
+        msg: "Only numbers please",
+      },
+      max: {
+        args: 100,
+        msg: "Maximum age is 100",
+      },
+      min: {
+        args: 15,
+        msg: "Minimum age is 15",
+      },
+      notNull: {
+        args: true,
+        msg: "Cannot be null",
+      },
+    },
   },
   from: {
     type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: {
+        args: true,
+        msg: "Only letters",
+      },
+      len: {
+        args: [3, 80],
+        msg: "Please write between 3 and 80 characters",
+      },
+    },
   },
 });
--- Api/src/models/tenant.model.js
+++ Api/src/models/tenant.model.js
@@ -10,16 +10,51 @@ export const Tenant = sequelize.define("Tenant", {
   fullName: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      isAlpha: {
+        args: true,
+        msg: "only letters",
+      },
+      min: {
+        args: 3,
+        msg: "the name at least needs 3 characters",
+      },
+      max: {
+        args: 60,
+        msg: "max characters are 60",
+      },
+      notNull: {
+        args: true,
+        msg: "cannot be null",
+      },
+    },
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
     validate: {
-      isEmail: true,
+      isEmail: {
+        args: true,
+        msg: "needs to be an email",
+      },
+      notNull: {
+        args: true,
+        msg: "cannot be null",
+      },
     },
   },
   avatar: {
     type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue:
+      "https://img2.freepng.es/20180418/wve/kisspng-refilmery-computer-icons-avatar-user-profile-avatar-vector-5ad7bb8f659906.3642332415240876954162.jpg",
+    validate: {
+      isUrl: {
+        args: true,
+        msg: "Please enter a URL",
+      },
+    },
   },
   password: {
     type: DataTypes.STRING,
@@ -27,8 +62,27 @@ export const Tenant = sequelize.define("Tenant", {
   phone: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    unique: true,
+    validate: {
+      isInt: {
+        args: true,
+        msg: "Only numbers please",
+      },
+      max: {
+        args: 15,
+        msg: "The maximum number of numbers are 15",
+      },
+      min: {
+        args: 11,
+        msg: "The minimum number of numbers are 11",
+      },
+    },
   },
   verify: {
     type: DataTypes.BOOLEAN,
+  },
+  verifyImg: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: true,
   },
 });
