bde8f6c5a2b8c9bb9330e640eeb5f3e9c7509e2d
Luciano Sanchez <cbrook3n@gmail.com>
2023-02-01 01:46:24 -0300

    valores del logging devueltos a default

--- Api/src/database/db.js
+++ Api/src/database/db.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 const sequelize = new Sequelize(
   "postgres://qgkajrlr:N8RxTLqSbOGGWvhM7wUG4_sCTRLQlr85@chunee.db.elephantsql.com/qgkajrlr",
   {
-    logging: false,
+    logging: true,
   }
 );
 
