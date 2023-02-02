64dbec0954a253bd514f46959469d1598855b87d
Felipe Duque <111407503+felduque@users.noreply.github.com>
2023-02-02 14:24:07 -0800

    Update

--- Api/index.js
+++ Api/index.js
@@ -4,7 +4,7 @@ let port = process.env.PORT || 3000;
 
 async function start() {
   try {
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ force: true });
     app.listen(port, () => {
       console.log("Server is running on port " + port);
     });
