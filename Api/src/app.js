0122a269cee08848ff4255e9faed4c8e9cdbab8d
Felipe Duque <111407503+felduque@users.noreply.github.com>
2023-02-02 00:27:16 -0800

    Revert "Nuevo Signup funcionando con backend. Ya registra a DB"
    This reverts commit 2a66aa702bfba501e8bdc698e237f2277375ad00.

--- Api/src/app.js
+++ Api/src/app.js
@@ -20,22 +20,6 @@ import path from "path";
 
 const app = express();
 
-import cors from 'cors';
-
-// Cors
-let allowedOrigins = ["http://127.0.0.1:5173"];
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 // Midelewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
