4b175654cd99c1e080e18a5bf333d2e8657cdeaa
fsamuelt <fsamuelt.bh@gmail.com>
2023-02-10 14:21:17 -0600

    Corrección para andar

--- client/src/components/NavBar/SearchBar/SearchBar.jsx
+++ client/src/components/NavBar/SearchBar/SearchBar.jsx
@@ -1,1 +1,12 @@
-//Necesitamos filtros
+export default function SearchBat() {
+    return (
+    <div className="search-input-bar">
+          <input type='text' className="input is-rounded is-small input-search" placeholder="Buscar por título..."/>
+          <div className="container-search-button">
+            <div className='button is-info is-outlined is-small is-rounded '>
+              <img src={searchIcon} className='search-button'/>
+            </div>
+          </div>
+        </div>
+    )
+};
