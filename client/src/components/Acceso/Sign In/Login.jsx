cf38ae8826c38bbb38bd016ee38d10e88239b9c6
Felipe Duque <111407503+felduque@users.noreply.github.com>
2023-02-13 02:41:30 -0500

    Update

--- client/src/components/Acceso/Sign In/Login.jsx
+++ client/src/components/Acceso/Sign In/Login.jsx
@@ -6,7 +6,8 @@ import usermailIcon from "../../../assets/usermail-login.png";
 import userPasswordIcon from "../../../assets/key-login.png";
 import leftarrow from "../../../assets/flecha-izquierda.png";
 import "./Login.css";
-import LoginGoogle from "./LoginGoogle";
+// import LoginGoogle from "./LoginGoogle";
+import Swal from "sweetalert2";
 
 //const LOGIN_URL = "/client/login";
 
@@ -16,12 +17,25 @@ export default function Login() {
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
+  const [setType, setSetType] = useState({
+    client: false,
+    tenant: false,
+  });
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   //const [role, setRole] = useState("");
 
   const [errMsg, setErrMsg] = useState("");
+
+  const handleChangeType = (e) => {
+    const { name } = e.target;
+    setSetType({
+      client: false,
+      tenant: false,
+      [name]: true,
+    });
+  };
 
   useEffect(() => {
     setErrMsg("");
@@ -42,10 +56,10 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (auth.role === "Client") {
+    if (setType.client === true) {
       try {
         const response = await axios.post(
-          `http://127.0.0.1:3000/client/login`,
+          `http://localhost:3000/client/login`,
           JSON.stringify({ email: email, password: password }),
           {
             headers: { "Content-Type": "application/json" },
@@ -83,10 +97,10 @@ export default function Login() {
           setErrMsg("Error al ingresar");
         }
       }
-    } else {
+    } else if (setType.tenant === true) {
       try {
         const response = await axios.post(
-          `http://127.0.0.1:3000/tenant/login`,
+          `http://localhost:3000/tenant/login`,
           JSON.stringify({ email: email, password: password }),
           {
             headers: { "Content-Type": "application/json" },
@@ -124,6 +138,14 @@ export default function Login() {
           setErrMsg("Error al ingresar");
         }
       }
+    } else {
+      Swal.fire({
+        title: "Login fallido",
+        text: "Algo salió mal, Marca tipo de usuario.",
+        icon: "error",
+        confirmButtonText: "Entendido",
+        timer: 4000,
+      });
     }
   };
   function back() {
@@ -191,8 +213,22 @@ export default function Login() {
                 <span>
                   <Link to="/register">Registrarme</Link>
                 </span>
-                <LoginGoogle />
+                {/* <LoginGoogle /> */}
               </p>
+              <button
+                name="client"
+                onClick={handleChangeType}
+                className="button is-link is-rounded"
+              >
+                soy Cliente
+              </button>
+              <button
+                name="tenant"
+                onClick={handleChangeType}
+                className="button is-link is-rounded"
+              >
+                Soy Arrendatario
+              </button>
             </section>
           </div>
         </div>
