05a690e92f2891bf26f496b9866657d3079612a7
Felipe Duque <111407503+felduque@users.noreply.github.com>
2023-02-16 11:17:08 -0500

    Update panel

--- client/src/components/Admin/AllPublish.jsx
+++ client/src/components/Admin/AllPublish.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { allPropierties } from "./Api";
+import { allPropierties, deleteProperty } from "./Api";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export const AllPublish = () => {
   const [allPropiertie, setAllPropiertie] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchAllPropierties = async () => {
@@ -11,28 +13,75 @@ export const AllPublish = () => {
       setAllPropiertie(allPropierte.data.result);
     };
     fetchAllPropierties();
-  }, []);
+  }, [allPropiertie.length]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, bórralo!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteProperty(id);
+        Swal.fire("Borrado!", "Tu archivo ha sido borrado.", "success");
+      }
+    });
+  };
 
   return (
     <div className="container-list-publish-tenant">
       <h2>Todas las publicaciones</h2>
+      <div className="container-list-publish-tenant__search">
+        <input
+          type="text"
+          className="input-list-publish-tenant"
+          placeholder="Buscar"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
       <div className="content-list-publish-all">
-        {allPropiertie.map((item) => {
-          return (
-            <Link to={`/propertyDetail/${item.id}`}>
+        {allPropiertie
+          .filter((val) => {
+            if (search === "") {
+              return val;
+            } else if (val.title.toLowerCase().includes(search.toLowerCase())) {
+              return val;
+            }
+          })
+          .map((item) => {
+            return (
               <div
                 key={item.id}
                 className="container-list-publish-tenant__list__item"
               >
-                <div className="container-list-publish-tenant__list__item__image">
-                  <img src={item.image[0]} alt={item.title} />
+                <div className="container-list-publish-tenant__list__item__delete">
+                  <button
+                    className="btn-delete-publish"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    X
+                  </button>
                 </div>
-                <h3 className="text-list-publish-title">{item.title}</h3>
-                <p className="text-list-publish-data">{item.date}</p>
+                <Link to={`/propertyDetail/${item.id}`}>
+                  <div className="container-list-publish-tenant__list__item__image">
+                    <img src={item.image[0]} alt={item.title} />
+                  </div>
+                  <h3 className="text-list-publish-title">
+                    {item.title
+                      .slice(0, 22)
+                      .concat(item.title.length > 22 ? "..." : "")}
+                  </h3>
+                </Link>
+                <p className="text-list-publish-data">{item.country}</p>
               </div>
-            </Link>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
