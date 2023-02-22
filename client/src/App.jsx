9b13b209f1c7a4c5753eab25ccd305d59cc9e589
fsamuelt <fsamuelt.bh@gmail.com>
2023-02-21 20:02:12 -0600

    Footer en todas direcciones menos en panel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,6 +29,7 @@ import ResumePay from "./components/ResumePay/ResumePay";
 import PaySuccess from "./components/MercadoPago/MPSuccess/MPSuccess";
 import PayFailure from "./components/MercadoPago/MPSuccess/MPSuccess";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
+import Footer from "././components/Footer/Footer";
 
 function App() {
   /*User Google*/
@@ -116,6 +117,7 @@ function App() {
           <Route path="*" element={<Home />} />
         </Routes>
       </div>
+      {location.pathname === "/settings" ? null : <Footer />}
     </div>
   );
 }
--- client/src/components/Pay/Suscribe.jsx
+++ client/src/components/Pay/Suscribe.jsx
@@ -2,7 +2,7 @@ import AuthContext from "../context/AuthProvider";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import PaymentMP from '../Pay/PaymentMP';
-import Footer from "../../components/Footer/Footer";
+
 import './Suscribe.css';
 import topPRO from '../../assets/pro-top.png';
 import chatPRO from '../../assets/pro-chat.png';
@@ -99,7 +99,7 @@ export default function Suscribe() {
           
         </div>
       </div>
-      <Footer />
+      
       </div>
     </>
   )
